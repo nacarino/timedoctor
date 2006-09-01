86f5c078ab78ae2252e7849272659a105b88ced3
ruttenm <ruttenm>
2006-09-01 11:28:19 +0000

    Initial version

---
+++ com.nxp.timedoctor.core.tests.model/src/com/nxp/timedoctor/core/tests/model/ISRSampleLineTest.java
@@ -0,0 +1,27 @@
+package com.nxp.timedoctor.core.tests.model;
+
+import junit.framework.TestCase;
+
+public class ISRSampleLineTest extends TestCase {
+
+	public void testAddSampleSampleTypeDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	public void testHasSamples() {
+		fail("Not yet implemented");
+	}
+
+	public void testInterruptTasks() {
+		fail("Not yet implemented");
+	}
+
+	public void testCalculate() {
+		fail("Not yet implemented");
+	}
+
+	public void testISRSampleLine() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ com.nxp.timedoctor.core.tests.model/src/com/nxp/timedoctor/core/tests/model/SectionListTest.java
@@ -0,0 +1,19 @@
+package com.nxp.timedoctor.core.tests.model;
+
+import junit.framework.TestCase;
+
+public class SectionListTest extends TestCase {
+
+	public void testAddSection() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSection() {
+		fail("Not yet implemented");
+	}
+
+	public void testValues() {
+		fail("Not yet implemented");
+	}
+
+}
