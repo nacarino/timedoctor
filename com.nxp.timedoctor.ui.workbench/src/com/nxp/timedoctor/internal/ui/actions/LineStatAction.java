dd6fb834d46c2c005eccefc19860e117487392b0
maheshdc <maheshdc>
2007-02-04 11:58:28 +0000

    Updated copyright notice and added images

--- com.nxp.timedoctor.ui.workbench/src/com/nxp/timedoctor/internal/ui/actions/LineStatAction.java
+++ com.nxp.timedoctor.ui.workbench/src/com/nxp/timedoctor/internal/ui/actions/LineStatAction.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2006 Royal Philips Electronics NV.
+ * Copyright (c) 2007 NXP Semiconductors B.V.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License version 1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Royal Philips Electronics NV. - initial API and implementation
+ *     NXP Semiconductors B.V. - initial API and implementation
  *******************************************************************************/
 package com.nxp.timedoctor.internal.ui.actions;
 
@@ -15,6 +15,7 @@ import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 import com.nxp.timedoctor.internal.ui.LineStatView;
 
@@ -29,7 +30,7 @@ public class LineStatAction extends Action implements IWorkbenchAction {
 		setId(ID);
 		setText("&Selected Row");
 		setToolTipText("Open the statistics view for the selected trace line.");
-		// TODO add image
+		setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin("com.nxp.timedoctor.ui", "icons/line_statistic_view.gif"));
 	}
 
 	@Override
