5e018d569ce595d59c01a5b845261b5d1083293d
maheshdc <maheshdc>
2007-07-11 08:59:43 +0000

    Moved ITimeDoctorWorkbenchConstants to ITimeDoctorUIConstants and to the plugin com.nxp.timedoctor.ui. Also, the icons and the images' folder names are stored inside this interface for use in other classes.

---
+++ com.nxp.timedoctor.ui/src/com/nxp/timedoctor/ui/ITimeDoctorUIConstants.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2007 NXP Semiconductors B.V.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License version 1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     NXP Semiconductors B.V. - initial API and implementation
+ *******************************************************************************/
+package com.nxp.timedoctor.ui;
+
+/**
+ * This interface stores constants for TimeDoctor
+ *
+ */
+public interface ITimeDoctorUIConstants {	
+	public static final String TASK_STATISTICS_VIEW     = "com.nxp.timedoctor.ui.workbench.LineStatView";
+	public static final String ALL_TASK_STATISTICS_VIEW = "com.nxp.timedoctor.ui.workbench.TraceStatView";
+	
+	public static final String TD_UI_PLUGIN = "com.nxp.timedoctor.ui";
+	
+	public static final String TOOLBAR_ENABLED_IMG_PATH = "icons/etool16/";
+	public static final String TOOLBAR_DISABLED_IMG_PATH = "icons/dtool16/";
+	public static final String LOCAL_TOOLBAR_ENABLED_IMG_PATH  = "icons/elcl16/";
+	public static final String LOCAL_TOOLBAR_DISABLED_IMG_PATH = "icons/dlcl16/";
+	
+	public static final String VIEWS_IMG_PATH = "icons/eview16/";
+}
