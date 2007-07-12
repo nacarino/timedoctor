027b2fe1571a672ba386150585459258ee69c2bf
maheshdc <maheshdc>
2007-07-12 07:34:03 +0000

    Added missing headers

--- com.nxp.timedoctor.product.workbench/src/com/nxp/timedoctor/product/workbench/actions/ManageConfigurationAction.java
+++ com.nxp.timedoctor.product.workbench/src/com/nxp/timedoctor/product/workbench/actions/ManageConfigurationAction.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2007 NXP Semiconductors B.V.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License version 1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     NXP Semiconductors B.V. - initial API and implementation
+ *******************************************************************************/
 package com.nxp.timedoctor.product.workbench.actions;
 
 import org.eclipse.jface.action.Action;
