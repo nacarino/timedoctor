71f6b0b6f67c4ad47ef417800a93442b86143796
maheshdc <maheshdc>
2007-07-24 14:38:23 +0000

    Added tee

--- com.nxp.timedoctor.build/workbench/product/run.sh
+++ com.nxp.timedoctor.build/workbench/product/run.sh
@@ -16,5 +16,5 @@ set PRODUCT_PATH=$PLUGIN_PATH/features/com.nxp.timedoctor.feature.workbench/work
 set JAVACSOURCE=1.5
 set JAVACTARGET=1.5
 
-java -jar $ECLIPSE_BASE_LOCATION/startup.jar -application org.eclipse.ant.core.antRunner -Dconfigs=$CONFIG -DarchivePrefix=TimeDoctor -DoutputUpdateJars=false -DbuildDirectory=$BUILD_DIRECTORY -Dbuilder=$BUILDER -Dproduct=$PRODUCT_PATH -DbaseLocation=$ECLIPSE_BASE_LOCATION -DbuildId=$BUILDID -DbuildType=$BUILDTYPE -DskipFetch=$SKIPFETCH -DpluginPath=$PLUGIN_PATH -DjavacSource=$JAVACSOURCE -DjavacTarget=$JAVACTARGET -buildfile $BUILDFILE > buildOutput-linux.log 2>&1
+java -jar $ECLIPSE_BASE_LOCATION/startup.jar -application org.eclipse.ant.core.antRunner -Dconfigs=$CONFIG -DarchivePrefix=TimeDoctor -DoutputUpdateJars=false -DbuildDirectory=$BUILD_DIRECTORY -Dbuilder=$BUILDER -Dproduct=$PRODUCT_PATH -DbaseLocation=$ECLIPSE_BASE_LOCATION -DbuildId=$BUILDID -DbuildType=$BUILDTYPE -DskipFetch=$SKIPFETCH -DpluginPath=$PLUGIN_PATH -DjavacSource=$JAVACSOURCE -DjavacTarget=$JAVACTARGET -buildfile $BUILDFILE 2>&1 | tee buildOutput-linux.log 
 
