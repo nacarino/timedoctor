dcae41bb1fbb7ba9f7d5317eb3704f0af9d8a327
maheshdc <maheshdc>
2007-07-25 10:53:40 +0000

    Removed set in run.sh and added " (quotes) to $CONFIG

--- com.nxp.timedoctor.build/workbench/product/run.sh
+++ com.nxp.timedoctor.build/workbench/product/run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-set ECLIPSE_BASE_LOCATION=
-set BUILDTYPE=I
-set BUILDID=TestBuild
-set SKIPFETCH=true
-set CONFIG="linux, gtk, x86"
+ECLIPSE_BASE_LOCATION=/psc/proj/ssd/Ipa/Projects/timedoctor/tools/linux/eclipse-SDK-3.2.2/eclipse
+BUILDTYPE=I
+BUILDID=TestBuild
+SKIPFETCH=true
+CONFIG="linux, gtk, x86"
 # set ARCHIVESFORMAT="linux, gtk, x86 - Folder"
 
 # YOU MAY NOT NEED TO EDIT ANYTHING BELOW THIS
-set BUILD_DIRECTORY=$PWD/build
-set PLUGIN_PATH=$PWD/build
-set BUILDER=$PWD/configuration
-set BUILDFILE=$PWD/build.xml
-set PRODUCT_PATH=$PLUGIN_PATH/features/com.nxp.timedoctor.feature.workbench/workbench.product
-set JAVACSOURCE=1.5
-set JAVACTARGET=1.5
+BUILD_DIRECTORY=$PWD/build
+PLUGIN_PATH=$PWD/build
+BUILDER=$PWD/configuration
+BUILDFILE=$PWD/build.xml
+PRODUCT_PATH=$PLUGIN_PATH/features/com.nxp.timedoctor.feature.workbench/workbench.product
+JAVACSOURCE=1.5
+JAVACTARGET=1.5
 
-java -jar $ECLIPSE_BASE_LOCATION/startup.jar -application org.eclipse.ant.core.antRunner -Dconfigs=$CONFIG -DarchivePrefix=TimeDoctor -DoutputUpdateJars=false -DbuildDirectory=$BUILD_DIRECTORY -Dbuilder=$BUILDER -Dproduct=$PRODUCT_PATH -DbaseLocation=$ECLIPSE_BASE_LOCATION -DbuildId=$BUILDID -DbuildType=$BUILDTYPE -DskipFetch=$SKIPFETCH -DpluginPath=$PLUGIN_PATH -DjavacSource=$JAVACSOURCE -DjavacTarget=$JAVACTARGET -buildfile $BUILDFILE 2>&1 | tee buildOutput-linux.log 
+java -jar $ECLIPSE_BASE_LOCATION/startup.jar -application org.eclipse.ant.core.antRunner -Dconfigs="$CONFIG" -DarchivePrefix=TimeDoctor -DoutputUpdateJars=false -DbuildDirectory=$BUILD_DIRECTORY -Dbuilder=$BUILDER -Dproduct=$PRODUCT_PATH -DbaseLocation=$ECLIPSE_BASE_LOCATION -DbuildId=$BUILDID -DbuildType=$BUILDTYPE -DskipFetch=$SKIPFETCH -DpluginPath=$PLUGIN_PATH -DjavacSource=$JAVACSOURCE -DjavacTarget=$JAVACTARGET -buildfile $BUILDFILE 2>&1 | tee buildOutput-linux.log 
 
